55dae735819cc8c3242db2edcf0146a92edb0711
Rohithsilent <shailajapullola@gmail.com>
2024-09-19 00:18:00 +0530

    Added customised parsing

--- main.py
+++ main.py
@@ -5,4 +5,23 @@ st.title("ScrapeAI")
 url = st.text_input("Enter a Website URL: ")
 
 if st.button("Scrape Site"):
-    st.write("scraping the Websit
+    st.write("scraping the Website")
+
+
+    result = scrape_website(url)
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = cleaned_content
+
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content",cleaned_content,height=300)
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse?")
+
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing the content")
+
+            dom_chunks = split_dom_content(st.session_state.dom_content)
